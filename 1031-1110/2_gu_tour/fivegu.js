e57e12a609856a25ddf3a693d3232263ee34827e
sjsjsjsue <esther106@naver.com>
2022-11-03 18:43:59 +0900

    feat : js 추가

---
+++ 1031-1110/2_gu_tour/fivegu.js
@@ -0,0 +1,23 @@
+// 클릭했을 때 상세정보 페이지 나오게 하기
+
+// API_KEY
+const travel_API_KEY =
+  'jlg7KY2npJeU37heElpwCwtYwftrL9yxya9arHvaWoAEmWZG5Pf8NZVOPG7QLy8LzpkAEjeNgey%2Fbj5Pv2N6uw%3D%3D';
+const travel_API_KEY2 =
+  '	jlg7KY2npJeU37heElpwCwtYwftrL9yxya9arHvaWoAEmWZG5Pf8NZVOPG7QLy8LzpkAEjeNgey/bj5Pv2N6uw==';
+const resturant_API_KEY =
+  'jlg7KY2npJeU37heElpwCwtYwftrL9yxya9arHvaWoAEmWZG5Pf8NZVOPG7QLy8LzpkAEjeNgey%2Fbj5Pv2N6uw%3D%3D';
+const resturant_API_KEY2 =
+  'jlg7KY2npJeU37heElpwCwtYwftrL9yxya9arHvaWoAEmWZG5Pf8NZVOPG7QLy8LzpkAEjeNgey/bj5Pv2N6uw==';
+// open api 여행지에 사용
+
+//  open api 맛집에 사용
+// 클릭했을 때 close 되는 함수
+function doClose() {
+  $('.spotsection').css('visibility', 'hidden');
+  $('.section-bottom').css('opacity', 1);
+}
+function doClose2() {
+  $('.spotsection').css('visibility', 'hidden');
+  $('.section-bottom').css('opacity', 1);
+}
